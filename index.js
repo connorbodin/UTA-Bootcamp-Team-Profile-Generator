3cc33327b36604485fb13ffb7ca2d3bd44d7eff6
connorbodin <connor.e.bodin@gmail.com>
2023-01-30 18:25:59 -0600

    added a lot, all prompts for user inputs with some validation(need to finsh validations) and constructors

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const inquirer = require('inquirer');
 const teamMembers = [];
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Employee = require('./lib/Employee');
+const Intern = require('./lib/Intern');
+const fs = require("fs");
+const { Console } = require('console');
+
+
+
 
 
 
@@ -10,12 +19,26 @@ const managerPrompt = () => {
         {
             type: 'input',
             name: 'managerName',
-            message: 'What is the name of the the manager?'
+            message: 'What is the name of the the manager?',
+            validate: managerNameInput => {
+                if(managerNameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a valid name!");
+                }
+            }
         },
         {
             type: 'input',
             name: 'managerId',
-            message: 'What is the ID number of the manager?'
+            message: 'What is the ID number of the manager?',
+            validate: managerIdInput => {
+                if(managerIdInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a valid Id!");
+                }
+            }
         },
         {
             type: 'input',
@@ -33,10 +56,122 @@ const managerPrompt = () => {
         console.log(answers);
         const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.mangerOffice);
         teamMembers.push(manager);
+        menuPrompt();
     });
 };
 
+const menuPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "employeeList",
+            message: "Which employee would you like to add next?",
+            choices: ['Engineer','Intern',"Finish building the team"]
+        }
+    ]).then(userChoice => {
+        switch (userChoice.employeeList) {
+            case "Engineer":
+                engineerPrompt();
+                break;
+            case "Intern":
+                internPrompt();
+                break;
+            default:
+                buildTeam();
+        }
+    });
+};
+
+const engineerPrompt = () => {
+    console.log(`"\x1b[1m",
+    ---------------------------- 
+        Building an Engineer
+    ----------------------------
+ 
+    `);
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the engineer?'
+        },
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'What is the id of the engineer?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the email of the engineer?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the github of the engineer?'
+        },
+
+    ]).then(answers => {
+        console.log(answers);
+        const engineer = new Engineer(answers.name, answers.employeeId, answers.email, answers.github);
+        teamMembers.push(engineer);
+        menuPrompt();
+    });
+};
+
+const internPrompt = () => {
+    console.log(`
+    ---------------------------- 
+        Building an Intern
+    ----------------------------
+ 
+    `);
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the intern?'
+        },
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'What is the id of the intern?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the email of the intern?'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the school of the intern?'
+        },
+
+    ]).then(answers => {
+        console.log(answers);
+        const intern = new Intern(answers.name, answers.employeeId, answers.email, answers.school);
+        teamMembers.push(intern);
+        menuPrompt();
+    });
+};
+
+
+const buildTeam = () => {
+    console.log(`
+    ---------------------------- 
+             Built team! 
+    ----------------------------
+`);
+
+console.log(teamMembers);
+
+
+
+
+}
+
+
 managerPrompt();
-
-console.log(teamMembers)
-
